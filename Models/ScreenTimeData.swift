bbbae0392c1c82c5d9a34d700471f0e167e61968
Ovsyannikov Mikhail <ovsyannikov.m10@wb.ru>
2025-09-04 18:47:58 +0300

    feat: add ScreenTimeData model with proper encapsulation

---
+++ Models/ScreenTimeData.swift
@@ -0,0 +1,47 @@
+//
+//  ScreenTimeData.swift
+//  MindfulApp
+//
+//  Created by Ovsyannikov.M10 on 04.09.2025.
+//
+
+import DataDetection
+
+final class ScreenTimeData {
+    let date = Date()
+
+    private(set) var screenTime: TimeInterval = 0
+    private(set) var unlockCount = 0
+
+    private var timeForApps: [String: TimeInterval] = [:]
+
+    var currentScreenTime: TimeInterval { screenTime }
+    var currentUnlockCount: Int { unlockCount }
+    var appUsage: [String: TimeInterval] { timeForApps }
+
+    init(
+        screenTime: TimeInterval,
+        unlockCount: Int = 0,
+        timeForApps: [String : TimeInterval]
+    ) {
+        self.screenTime = screenTime
+        self.unlockCount = unlockCount
+        self.timeForApps = timeForApps
+    }
+}
+
+extension ScreenTimeData {
+    func setUnlockCount(_ unlockCount: Int) {
+        guard unlockCount >= 0 else { return }
+        self.unlockCount = unlockCount
+    }
+
+    func updateScreenTime(_ screenTime: TimeInterval) {
+        self.screenTime += screenTime
+    }
+
+    func addScreenTime(_ app: String, _ time: TimeInterval) {
+        guard !app.isEmpty, time >= 0 else { return }
+        timeForApps[app, default: 0] += time
+    }
+}
